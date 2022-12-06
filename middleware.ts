fe72accbf7047c615a69488f43eb985f2a7a07ba
Dan Burnside <daniel.burnside@daveramsey.com>
2022-12-06 15:49:16 -0600

    remove console log

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ const signedinPages = ["/", "/playlist", "/library"];
 
 export default function middleware(req) {
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
-    console.log("match", req.cookies.get("TRAK_ACCESS_TOKEN"));
     const url = req.nextUrl.clone();
     const token = req.cookies.get("TRAK_ACCESS_TOKEN");
     if (!token) {
