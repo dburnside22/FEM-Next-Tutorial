c9f24d57978e77082b2b72fdc42989d0f4d2d5fc
Dan Burnside <daniel.burnside@daveramsey.com>
2022-12-06 14:36:18 -0600

    implement auth

---
+++ pages/signin.tsx
@@ -0,0 +1,9 @@
+import AuthFrom from "../components/authform";
+
+const Signin = () => {
+  return <AuthFrom mode="signin" />;
+};
+
+Signin.authPage = true;
+
+export default Signin;
---
+++ pages/signup.tsx
@@ -0,0 +1,9 @@
+import AuthFrom from "../components/authform";
+
+const Signup = () => {
+  return <AuthFrom mode="signup" />;
+};
+
+Signup.authPage = true;
+
+export default Signup;
